dab96e15a9c08696b3aafa55494fc085d827362d
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-10-31 14:25:54 +0100

    Modify instance_ double and withdraw method, write a test and method for 'expired card'

--- lib/atm.rb
+++ lib/atm.rb
@@ -4,33 +4,41 @@ class Atm
     @funds = 1000
   end
 
-def withdraw(amount, pin_code, account)
-  case
-  when insufficient_funds_in_account?(amount, account)
-    { status: false, message: 'insufficient funds in account', date: Date.today }
-  when insufficient_funds_in_atm?(amount)
-    { status: false, message: 'insufficient funds in ATM', date: Date.today }
-  when incorrect_pin?(pin_code, account.pin_code)
-    { status: false, message: 'wrong pin', date: Date.today }
-  else
-    perform_transaction(amount, account)
+  def card_expired?(exp_date)
+    Date.strptime(exp_date, '%m/%y') < Date.today
   end
-end
 
-private
+  def withdraw(amount, pin_code, account)
+    if insufficient_funds_in_account?(amount, account)
+      { status: false, message: 'insufficient funds in account', date: Date.today }
+    elsif insufficient_funds_in_atm?(amount)
+      { status: false, message: 'insufficient funds in ATM', date: Date.today }
+    elsif incorrect_pin?(pin_code, account.pin_code)
+      { status: false, message: 'wrong pin', date: Date.today }
+    elsif card_expired?(account.exp_date)
+      { status: false, message: 'card expired', date: Date.today }
+    else
+      perform_transaction(amount, account)
+    end
+  end
 
-def incorrect_pin?(pin_code, actual_pin)
-  pin_code != actual_pin
-end 
-def insufficient_funds_in_atm?(amount)
-  @funds < amount
-end
-def insufficient_funds_in_account?(amount, account)
-  amount > account.balance
-end
-def perform_transaction(amount, account)
-  @funds -= amount
+  private
+
+  def incorrect_pin?(pin_code, actual_pin)
+    pin_code != actual_pin
+  end
+
+  def insufficient_funds_in_atm?(amount)
+    @funds < amount
+  end
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @funds -= amount
     account.balance = account.balance - amount
     { status: true, message: 'success', date: Date.today, amount: 45 }
-end
+  end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,11 +1,11 @@
 require './lib/atm.rb'
 require 'date'
 describe Atm do
-  let(:account) { instance_double('Account', pin_code: '1234') }
-before do
-  allow(account).to receive(:balance).and_return(100)
-  allow(account).to receive(:balance=)
-end
+  let(:account) { instance_double('Account', pin_code: '1234', exp_date: '01/18') }
+  before do
+    allow(account).to receive(:balance).and_return(100)
+    allow(account).to receive(:balance=)
+  end
   it 'has 1000$ on initialize' do
     expect(subject.funds).to eq 1000
   end
@@ -29,5 +29,10 @@ end
   it 'allow withdraw if account has enough balance.' do
     expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
     expect(subject.withdraw(45, '1234', account)).to eq expected_output
+  end
+  it 'rejects withdraw if card is expired' do
+    allow(account).to receive(:exp_date).and_return('12/15')
+    expected_output = { status: false, message: 'card expired', date: Date.today }
+    expect(subject.withdraw(6, '1234', account)).to eq expected_output
   end
 end
