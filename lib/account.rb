385ee934439df51c56457e60742206f6b411c769
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-02 10:25:16 +0100

    Add account_status to Account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,16 @@
 class Account
   STANDARD_VALIDITY_YRS = 5
- attr_accessor :pin_code, :balance
+ attr_accessor :pin_code, :balance, :account_status
   def initialize
     @pin_code = generate_pin
     @balance = 0
+    @account_status = :active
+  end
 
     def exp_date
       Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
-  end
+
     private
     def generate_pin
       rand(1000..9999)
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -15,4 +15,8 @@ describe Account do
     expected_date = Date.today.next_year(5).strftime('%m/%y')
     expect(subject.exp_date).to eq expected_date
   end
+  
+  it 'is expected to have :active status on initialize' do
+    expect(subject.account_status).to eq :active
+  end
 end
