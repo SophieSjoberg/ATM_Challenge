d3de12bdd1e678620f2c9d12f442d97533f32cda
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-02 10:12:38 +0100

    Add exp_date to Account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,16 +1,15 @@
 class Account
+  STANDARD_VALIDITY_YRS = 5
  attr_accessor :pin_code, :balance
   def initialize
     @pin_code = generate_pin
     @balance = 0
+
+    def exp_date
+      Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    end
   end
-    #it 'check length of a number' do
-    #number = 1234
-    #number_length = Math.log10(number).to_i + 1
-    #expect(number_length).to eq 4
-
     private
-
     def generate_pin
       rand(1000..9999)
     end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,7 @@
 require './lib/account.rb'
+require 'date'
 
 describe Account do
-
   it 'is expected to have a 4 digit pin number on initialize' do
     pin_length = Math.log10(subject.pin_code).to_i + 1
     expect(pin_length).to eq 4
@@ -10,6 +10,9 @@ describe Account do
   it 'is expected to have a balance of 0 on initialize' do
     expect(subject.balance).to eq 0
   end
-end
 
-  #let(:account) { instance_double(account_owner: 'Owner', pin_code: '1234', exp_date: '01/18', balance: 1000, account_status: :active) }
+  it 'is expected to have an expiry date on initialize' do
+    expected_date = Date.today.next_year(5).strftime('%m/%y')
+    expect(subject.exp_date).to eq expected_date
+  end
+end
